798308ce7cc7f49dda550313d0372ffbb2ef8064
Asd-g <65298684+Asd-g@users.noreply.github.com>
2023-06-28 01:04:23 +0300

    Add missing headers

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,6 +1,6 @@
 #include "avs_libplacebo.h"
 
-static const char* AVSC_CC avisynth_c_plugin_init(AVS_ScriptEnvironment* env)
+const char* AVSC_CC avisynth_c_plugin_init(AVS_ScriptEnvironment* env)
 {
     avs_add_function(env, "libplacebo_Deband", "c[iterations]i[threshold]f[radius]f[grainY]f[grainC]f[dither]i[lut_size]i[temporal]b[planes]i*[device]i[list_device]b[grain_neutral]f*", create_deband, 0);
     avs_add_function(env, "libplacebo_Resample", "cii[filter]s[radius]f[clamp]f[taper]f[blur]f[param1]f[param2]f[sx]f[sy]f[antiring]f[lut_entries]i[cutoff]f[sigmoidize]b[linearize]b[sigmoid_center]f[sigmoid_slope]f[trc]i[cplace]i[device]i[list_device]b[src_width]f[src_height]f", create_resample, 0);
--- src/shader.cpp
+++ src/shader.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <mutex>
 #include <regex>
 
